2e4a8ec9576334c99baa6ecaeca85af37ee4651d
Thomas Berthé <berthe.thomas01@gmail.com>
2017-10-02 11:59:27 +0200

    controller weather done

---
+++ app/controllers/weather.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+	router = express.Router(),
+	Weather = require('../models/weather'),
+  global = require('../global');
+
+router.get('/weather', function(req, res, next){
+
+	Weather.getWeather(function(weather){
+		res.render('weather', {weather: weather});
+	})
+});
+
+module.exports = router;
---
+++ app/global.js
@@ -0,0 +1,2 @@
+var API_KEY = '0271a5fc4a61c6cceaadc15648c2ab97';
+var ID_CITY = '6434483';
